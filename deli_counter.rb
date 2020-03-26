0a621fdea67fff8025014ac6aef7522885a8e738
Ray Alva <rayalva407@gmail.com>
2020-03-26 20:01:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,8 +21,8 @@ def take_a_number(array, name)
 end
 
 def now_serving(array)
-  while array.length > 0
+  if array.length > 0
     puts "Currently serving #{array.shift()}."
-  end
+  else
   puts "There is nobody waiting to be served!"
 end
